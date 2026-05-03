4d76058aaae579bb782b2ea81dc503328e9673ef
a2025148289@isec.pt <a2025148289@isec.pt>
2026-05-03 16:25:22 +0100

    pre-release da 3_3_a RN implementacao efunc_feedforward antes de paralelizar o codigo

--- matlab/tp_3_3_a_RN_implementacao.m
+++ matlab/tp_3_3_a_RN_implementacao.m
@@ -5,7 +5,8 @@
 % parametros quanto aos dados de entrada
 %type_imput = ["Median" , "MICE"]; 
 t_imput   = "MICE"              ; % tipo de imputaçao de fill nans
-type_data = [ "ORIG"  , "NORM"] ; %tipos de dados - originais ou normalizado
+%type_data = [ "ORIG"  , "NORM"]
+t_data = "NORM" ; % tipos de dados - originais ou normalizado
 
 % parametros quanto à topologia das redes neuronais
 topology = {10; [5 5]; 6; [3 3]; 12; [6 6]; [4 4 4]} ;  
@@ -16,7 +17,7 @@ transf_fun_hid = ["poslin" , "logsig"  , "tansig"];
 transf_fun_out = ["purelin", "logsig" ];
 
 % proporçoes de treino/validaçao/teste
-data_split_proportions = {[0.7 0.15 0.15], [0.7 0.2 0.1], [0.9, 0.05, 0.05]};
+data_split_proportions = {[0.7 0.15 0.15], [0.7 0.2 0.1], [0.9 0.05 0.05]};
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -83,39 +84,71 @@ end
 tabCaseLib.(target_col) = [];
 
 
-for t_data = type_data
+if t_data == "NORM"
+    % rescale dos datasets treino e de teste (apenas att numericos)
+    % so os attributos numericos senao da' cabo das matrizes sim
+    cols_min = dict_att_min(att_cols);
+    cols_max = dict_att_max(att_cols);
+    tabCaseLib{:,att_cols} = normalize_values(tabCaseLib{:,att_cols}, cols_min, cols_max);
+end
 
-    if t_data == "NORM"
-        % rescale dos datasets treino e de teste (apenas att numericos)
-        % so os attributos numericos senao da' cabo das matrizes sim
-        cols_min = dict_att_min(att_cols);
-        cols_max = dict_att_max(att_cols);
-        tabCaseLib{:,att_cols} = normalize_values(tabCaseLib{:,att_cols}, cols_min, cols_max);
-    end
+nn_case.type_imp = t_imput;
+nn_case.type_data = t_data;
 
-    nn_setup.input_layer  = transpose(tabCaseLib{:,att_cols});
-    nn_setup.output_layer = transpose(tabCaseLib{:,target_outputs});
-    
-    for topo = transpose(topology)
-        nn_setup.topology = topo;
+nn_case.input_layer  = tabCaseLib{:,att_cols};
+nn_case.output_layer = tabCaseLib{:,target_outputs};
 
-        for trainf = training_fun
-            nn_setup.training_fun = trainf;
+results_lst = {};
+results_idx = 0; %incrementada antes de ser usada
+for topo = transpose(topology)
+    nn_case.topology = cell2mat(topo);
 
-            for transffHid = transf_fun_hid
-                nn_setup.transf_fun_hid = transffHid;
+    for trainf = training_fun
+        nn_case.training_fun = trainf;
 
-                for transffOut = transf_fun_out
-                    nn_setup.transf_fun_out = transffOut;
+        for transffHid = transf_fun_hid
+            nn_case.transf_fun_hid = transffHid;
+
+            for transffOut = transf_fun_out
+                nn_case.transf_fun_out = transffOut;
+                
+                for proportions = data_split_proportions
+                    nn_case.data_split = cell2mat(proportions);
                     
-                    for proportions = data_split_proportions
-                        nn_setup.data_split = proportions;
-
-                        for i = 1 : 10
-                            results = nn_ff(nn_setup);
-                        end
-
+                    nn_case.case_name = gen_case_name(nn_case);
+                    
+                    sum_acc_val = 0;
+                    sum_acc_test = 0;
+                    num_runs = 10;
+                    for i = 1 : num_runs
+                        nn_case.num_run = i;
+                        [~, acc_val, acc_test] = nn_ff(nn_case, true, false);
+                        sum_acc_val = sum_acc_val + acc_val;
+                        sum_acc_test = sum_acc_test + acc_test;
                     end
+
+                    avg_acc_val = sum_acc_val / num_runs;
+                    avg_acc_test = sum_acc_test / num_runs;
+                    
+                    % escreve numa tabelea os resultados
+                    topo_str = mat2str(nn_setup.topology);
+                    split_str = mat2str(nn_setup.data_split);
+
+                    results_idx = results_idx + 1;
+                    results_lst(results_idx, :) = {   ...
+                                                        nn_setup.case_name, ...
+                                                        nn_setup.type_imp, ...
+                                                        nn_setup.type_data, ...
+                                                        topo_str, ...
+                                                        nn_setup.training_fun, ...
+                                                        nn_setup.transf_fun_hid, ...
+                                                        nn_setup.transf_fun_out, ...
+                                                        split_str, ...
+                                                        avg_acc_val, ...
+                                                        avg_acc_test ...
+                                                     };
+                    
+                    
                 end
             end
         end
@@ -123,5 +156,34 @@ for t_data = type_data
 
 end
 
+res_col_names = {'Case_Name', 'Imputacao', 'Dados', 'Topologia', ...
+                 'Treino_Func', 'Transf_Hid', 'Transf_Out', ...
+                 'Divisao_Dados', 'Media_Acc_Validacao', 'Media_Acc_Teste'};
+            
+
+tab_results = cell2table(results_lst, 'VariableNames', res_col_names);
+excel_out = output_folder_path + "Resultados_Estudo_Parametrico.xlsx";
+writetable(tab_results, ficheiro_excel_out);
+
+fprintf("\n\nEstudo Parametrico concluido e dados exportados com sucesso.\n\n")
 
 
+function [name] = gen_case_name(nn)
+
+    name = "RN_" + nn.type_imp + "_" + nn.type_data + "_Topo";
+
+    for n = nn.topology
+        name = name + "-" + n;
+    end
+    
+    name = name + "_Train-" + nn.training_fun;
+
+    name = name + "_TransfHid-" + nn.transf_fun_hid;
+
+    name = name + "_TransfOut-" + nn.transf_fun_out + "_Prop";
+    
+    %proportions
+    for n = nn.data_split
+        name = name + "-" + n;
+    end
+end
--- matlab/tp_func_feedforwardNN.m
+++ matlab/tp_func_feedforwardNN.m
@@ -1,5 +1,121 @@
-function [net] = tp_func_feedforwardNN(neural_network_setup)
+function [net, acc_val, acc_test] = tp_func_feedforwardNN(neural_network_setup, hidden_calc, print_figs)
+    
+    %%%%%%%%%
+    % SETUP %
+    %%%%%%%%%
+    
+    case_name = neural_network_setup.case_name + "_R-" + neural_network_setup.num_run;
+    
+    fprintf("\nA correr: %s\n", case_name)
 
-    net = feedforwardnet();
+    % bloquear  
+    if hidden_calc
+        set(0, 'DefaultFigureVisible', 'off');
+    end
+
+    inp_layer = neural_network_setup.input_layer;   
+    out_layer = neural_network_setup.output_layer;
+
+    if size(inp_layer,1) == size(out_layer,1)
+        inp_layer = transpose(inp_layer);
+        out_layer = transpose(out_layer);
+    end
+    
+    num_hid_layers = length(neural_network_setup.topology);
+
+    net = feedforwardnet(   neural_network_setup.topology, ...
+                            neural_network_setup.training_fun   );
+    
+    % divisao do dataset
+    net.divideFcn = 'dividerand'; % por defeito, p/ todos os casos
+    net.divideParam.trainRatio = neural_network_setup.data_split(1);
+    net.divideParam.valRatio   = neural_network_setup.data_split(2);
+    net.divideParam.testRatio  = neural_network_setup.data_split(3);
+
+    % atribuiçao da func de ativacao 'a(s) camada(s) escondida(s)
+    
+    for i = 1: num_hid_layers
+        net.layers{i}.transferFcn = neural_network_setup.transf_fun_hid;
+    end
+    
+    % funcs de ativacao da(s) escondida(s)
+    net.layers{num_hid_layers + 1}.transferFcn = neural_network_setup.transf_fun_out;
+
+   
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % TREINO COM SUB-DATASET DE TREINO VS SUB-DATSET DE VALIDACAO %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    if hidden_calc
+        net.trainParam.showWindow = false;
+    end
+
+    [net,tr] = train(net, inp_layer, out_layer);
+    
+    out_val  = sim(net, inp_layer);
+    
+    if print_figs
+        plotconfusion(out_layer, out_val);
+        fig_conf = gcf;
+        conf_mat_path = "PlotConfusao_"+ case_name + ".png" ;
+        exportgraphics(fig_conf, conf_mat_path, 'Resolution', 300);
+        close(fig_conf);
+
+        plotperf(tr);
+        fig_perf = gcf;
+        perf_path = "PlotPerform_"+ case_name + ".png" ;
+        exportgraphics(fig_perf, perf_path, 'Resolution', 300);
+        close(fig_perf);
+    end
+
+    inp_layer_val = inp_layer(:, tr.valInd);
+    out_layer_val = out_layer(:, tr.valInd);
+    out_val = sim(net, inp_layer_val);
+    
+    error_val = perform(net, out_val, out_layer_val);
+    acc_val   = accuracy(out_val, out_layer_val);
+
+    fprintf("\nErro na classificaçao (fase de validaçao) = %f\n", error_val);
+    fprintf("Precisao total (fase de validaçao) = %f\n", acc_val);
+
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % VALIDAÇAO FINAL VS SUB-DATSET DE TESTE %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    inp_layer_test = inp_layer(:, tr.testInd);
+    out_layer_test = out_layer(:, tr.testInd);
+
+    out_test = sim(net, inp_layer_test);
+
+    error_test = perform(net, out_test, out_layer_test);
+    acc_test   = accuracy(out_test, out_layer_test);
+    fprintf('\nErro na classificaçao (fase de teste) = %f\n', error_test)
+    fprintf("Precisao total (fase de teste) = %f\n", acc_test)
+
+    % reativa plot de figuras
+    set(0, 'DefaultFigureVisible', 'on');
+end
+
+
+
+function [acc] = accuracy(target_predict, target_real)
+
+    %Calcula e mostra a percentagem de classificacoes corretas
+
+    r=0;
+    for i=1:size(target_predict,2)
+
+        % Para cada classificacao  
+        [ ~ , b ] = max( target_predict(:,i) );  % b guarda a linha onde encontrou valor mais alto da saida obtida
+        [ ~ , d ] = max(    target_real(:,i) );  % d guarda a linha onde encontrou valor mais alto da saida desejada
+        
+        % se estao na mesma linha, a classificacao foi correta (incrementa 1)
+        if b == d
+            r = r + 1;
+        end
+    end
+
+    acc = r / size(target_predict,2) * 100;
 
 end
