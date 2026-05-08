542ff99ebe13b736674d1444f22f18218938bd46
Miguel Serra <miguel.serra.mspe@gmail.com>
2026-05-09 00:43:25 +0100

    parelizacao do script tp_3_3_b

--- matlab/tp_3_3_b_RN_dataset_bruto_vs_norm.m
+++ matlab/tp_3_3_b_RN_dataset_bruto_vs_norm.m
@@ -15,11 +15,12 @@ num_reps_nn = 30;
 % >>> REVER SEMPRE ESTE SETOR QUANDO SE ALTERAREM FUNÇOES!!!!! <<< %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 addpath('functions')
-get_file         = @tp_func_get_xlfile      ;
-normalize_values = @tp_func_rescale_2         ;
-denorm_values    = @tp_func_rescale_reverse_2 ;
-nn_ff            = @tp_func_feedforwardNN   ;
-categ2cols       = @tp_func_categ2cols      ;
+get_file         = @tp_func_get_xlfile         ;
+normalize_values = @tp_func_rescale_2          ;
+denorm_values    = @tp_func_rescale_reverse_2  ;
+nn_ff            = @tp_func_feedforwardNN      ;
+categ2cols       = @tp_func_categ2cols         ;
+
 
 %%%%%%%%%%%%%%%%%%%%%%%
 % PREPARAÇAO DE DADOS %
@@ -48,7 +49,6 @@ tabResultsNN = readtable(nn_res_file_path);
 % prepara as pastas e nomes comuns via script aux
 tp_3_0_setup_common;
 
-
 % Transforma a coluna do target em 3 colunas binarias (num de outputs)
 [tabCaseLib_base, target_outputs] = categ2cols(tabCaseLib, target_col);
 
@@ -61,13 +61,14 @@ tab_results_sorted = sortrows(tabResultsNN, {'avg_acc_test', 'avr_err_test'}, {'
 neural_networks_topScorers = tab_results_sorted(1:3, :);
 neural_networks_botScorers = tab_results_sorted(end-2:end, :);
 
+% mapeia uma coluna a dizer de é top ou bot scorer
 neural_networks_topScorers.rank = repmat("topScorer", 3, 1);
 neural_networks_botScorers.rank = repmat("botScorer", 3, 1);
 
 % combina numa tabela unica
 neural_networks = [neural_networks_topScorers; neural_networks_botScorers];
 
-% neural_networks = neural_networks(:,col_names);
+% corrige a importaçao do array que em formato string
 neural_networks.topology   = cellfun(@str2num, neural_networks.topology, 'UniformOutput', false);
 neural_networks.data_split = cellfun(@str2num, neural_networks.data_split, 'UniformOutput', false);
 
@@ -78,93 +79,105 @@ neural_networks.data_split = cellfun(@str2num, neural_networks.data_split, 'Unif
 
 fprintf("\n\nTarefa: REDES NEURONAIS ORIG vs NORM --- A Iniciar..\n\n");
 
-num_cases = size(neural_networks, 1) * 2; % (3 + 3) 'casos' * 2 'orig/norm'
-results_lst = cell(num_cases, 17);
-idx = 0;
 
+case_list = {};
 for i = 1:height(neural_networks)
-    
+
     nn_case = table2struct(neural_networks(i, :));
 
     for t_data = type_data
-        
-
-        tabCaseLib = tabCaseLib_base; % trabalha sobre copia do dataset
-
-        if t_data == "NORM"
-            cols_min = dict_att_min(att_cols);
-            cols_max = dict_att_max(att_cols);
-            tabCaseLib{:, att_cols} = normalize_values(tabCaseLib{:, att_cols}, cols_min, cols_max);
-        end
-    
-        nn_case.input_layer  = tabCaseLib{:,att_cols};
-        nn_case.output_layer = tabCaseLib{:,target_outputs};
-
-
-        sum_acc_glob   = 0; sum_acc_test   = 0; 
-        sum_err_glob   = 0; sum_err_test   = 0;
-        sum_num_epochs = 0; sum_best_epoch = 0;
-        sum_tr_time    = 0;
-
-        best_err_nn = Inf;
-        best_nn = [];
-        for n = 1 : num_reps_nn
-            nn_case.rep_num = n;
-            [nn_ff_out] = nn_ff(nn_case, true, false);
-            sum_acc_test = sum_acc_test + nn_ff_out.acc_test;
-            sum_err_test = sum_err_test + nn_ff_out.err_test; 
-
-            % guarda a repetiçao da rede com menor erro MSE
-            if nn_ff_out.err_test < best_err_nn
-                best_err_nn = nn_ff_out.err_test;
-                best_nn = nn_ff_out;
-            end
-        end
-
-
-        % calcula as medias
-        avg_acc_test = sum_acc_test / num_reps_nn;
-        avg_err_test = sum_err_test / num_reps_nn;
-        
-        topo_str  = mat2str(nn_case.topology);
-        split_str = mat2str(nn_case.data_split);
-        
-        
-        idx = idx + 1;
-        results_lst(idx, :) = { nn_case.rank,             ...
-                                nn_case.case_name,      ...
-                                nn_case.type_imp,       ...
-                                nn_case.type_data,      ...
-                                topo_str,               ...
-                                nn_case.training_fun,   ...
-                                nn_case.transf_fun_hid, ...
-                                nn_case.transf_fun_out, ...
-                                nn_case.max_fail,     ...
-                                split_str,              ...
-                                avg_err_glob,           ...
-                                avg_err_test,           ...
-                                avg_acc_glob,           ...
-                                avg_acc_test,           ...
-                                avg_num_epochs,         ...
-                                avg_best_epoch,         ...
-                                avg_tr_time             ...
-                            };
-
-
-        fig_conf = plotconfusion(best_nn.out_layer_test, best_nn.out_predict_test); 
-        conf_mat_path = output_folder_path + "/plot_confusao_" + nn_case.rank + "_" + nn_case.case_name + "_" + t_data +  ".png" ;
-        exportgraphics(fig_conf, conf_mat_path, 'Resolution', 300);
-        close(fig_conf);
-
+        nn_case.type_data = t_data;
+        case_list{end+1} = nn_case; 
     end
 
 end
 
+num_cases = length(case_list);
+results_lst = cell(num_cases, 15);
+parfor i = 1:num_cases
+    
+    curr_nn = case_list{i}; 
 
-res_col_names = {'case_name', 'type_imp', 'type_data', 'topology', ...
+    tabCaseLib = tabCaseLib_base; % trabalha sobre copia do dataset
+
+    if t_data == "NORM"
+        cols_min = dict_att_min(att_cols);
+        cols_max = dict_att_max(att_cols);
+        tabCaseLib{:, att_cols} = normalize_values(tabCaseLib{:, att_cols}, cols_min, cols_max);
+    end
+
+    curr_nn.input_layer  = tabCaseLib{:,att_cols};
+    curr_nn.output_layer = tabCaseLib{:,target_outputs};
+
+    sum_acc_test   = 0; 
+    sum_err_test   = 0;
+    sum_num_epochs = 0;
+    sum_best_epoch = 0;
+    sum_tr_time    = 0;
+
+    best_err_nn = Inf;
+    best_nn = [];
+    for n = 1 : num_reps_nn
+
+        curr_nn.rep_num = n;
+
+        nn_ff_out = nn_ff(curr_nn, true, false);
+        
+        sum_acc_test   = sum_acc_test   + nn_ff_out.acc_test;
+        sum_err_test   = sum_err_test   + nn_ff_out.err_test; 
+        sum_num_epochs = sum_num_epochs + nn_ff_out.num_epochs;
+        sum_best_epoch = sum_best_epoch + nn_ff_out.best_epoch; 
+        sum_tr_time    = sum_tr_time    + nn_ff_out.tr_time;
+
+        % guarda a repetiçao da rede com menor erro MSE
+        if nn_ff_out.err_test < best_err_nn
+            best_err_nn = nn_ff_out.err_test;
+            best_nn = nn_ff_out;
+        end
+    end
+
+
+    % calcula as medias
+    avg_acc_test   = sum_acc_test   / num_reps_nn;
+    avg_err_test   = sum_err_test   / num_reps_nn;
+    avg_num_epochs = sum_num_epochs / num_reps_nn; 
+    avg_best_epoch = sum_best_epoch / num_reps_nn;  
+    avg_tr_time    = sum_tr_time    / num_reps_nn; 
+    
+    topo_str  = mat2str(curr_nn.topology);
+    split_str = mat2str(curr_nn.data_split);
+    
+    results_lst(i, :) = { curr_nn.rank,             ...
+                          curr_nn.case_name,      ...
+                          curr_nn.type_imp,       ...
+                          curr_nn.type_data,      ...
+                          topo_str,               ...
+                          curr_nn.training_fun,   ...
+                          curr_nn.transf_fun_hid, ...
+                          curr_nn.transf_fun_out, ...
+                          split_str,              ...
+                          curr_nn.epochs_max_fail,...
+                          avg_err_test,           ...
+                          avg_acc_test,           ...
+                          avg_num_epochs,         ...
+                          avg_best_epoch,         ...
+                          avg_tr_time             ...
+                        };
+
+
+    fig_conf = plotconfusion(best_nn.out_layer_test, best_nn.out_predict_test); 
+    conf_mat_path = output_folder_path + "/plot_confusao_" + curr_nn.rank + "_" ...
+                    + curr_nn.case_name + "_" + curr_nn.type_imp + " _ " + ...
+                    curr_nn.type_data +  ".png" ;
+    exportgraphics(fig_conf, conf_mat_path, 'Resolution', 300);
+    close(fig_conf);
+
+end
+
+
+res_col_names = {'rank', 'case_name', 'type_imp', 'type_data', 'topology', ...
                  'training_fun', 'transf_fun_hid', 'transf_fun_out', ...
-                 'data_split', 'epochs_max_fail',' avg_err_global',  ...
-                 'avr_err_test', 'avg_acc_global', 'avg_acc_test',  ...
+                 'data_split', 'epochs_max_fail', 'avr_err_test', 'avg_acc_test', ...
                  'avg_num_epochs', 'avg_best_epoch', 'avg_tr_time'};
             
 
